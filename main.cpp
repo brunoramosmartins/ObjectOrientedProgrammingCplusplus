8f129483c4cde3bf3df6de46b4df97891dda9173
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-07-06 11:05:34 -0300

    Problemas no ambiente de desenvolvimento.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     // 1 print help
     std::cout << "1: Print help " << std::endl;
     // 2 print exchange stats
-    std::cout << "2: Print exchange stats " << std::endl;
+    std::cout << "2: Print exchange stats" << std::endl;
     // 3 make an offer
     std::cout << "3: Make an offer " << std::endl;
     // 4 make a bid
