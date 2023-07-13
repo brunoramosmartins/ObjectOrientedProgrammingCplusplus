2f6d044b54410a6f759862bcd5eb27727646f8a5
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-07-13 17:06:54 -0300

    Revisão de c++.

---
+++ Week2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void printMenu() {
+
+            // 1 print help
+            std::cout << "1: Print help " << std::endl;
+            // more options here
+}
+
+int main () {
+        
+    while (true) {
+           printMenu();
+           // user input capture and processing code here... 
+    }
+    return 0;
+}
